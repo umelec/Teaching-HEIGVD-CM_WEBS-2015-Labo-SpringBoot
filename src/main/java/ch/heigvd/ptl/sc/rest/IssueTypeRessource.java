99731598359481afedc13eeff76d2b28ce5ef0af
robertdirosa <robert@di-rosa.ch>
2015-02-18 15:30:30 +0100

    Squeleton for Action and issuetypes

---
+++ src/main/java/ch/heigvd/ptl/sc/rest/IssueTypeRessource.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ch.heigvd.ptl.sc.rest;
+
+import ch.heigvd.ptl.sc.CityEngagementException;
+import ch.heigvd.ptl.sc.persistence.UserRepository;
+import ch.heigvd.ptl.sc.converter.UserConverter;
+import ch.heigvd.ptl.sc.model.User;
+import ch.heigvd.ptl.sc.to.UserTO;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+/**
+ *
+ * @author Robert
+ */
+@Component
+@Path("/issuestypes")
+public class IssueTypeRessource {
+    
+}
