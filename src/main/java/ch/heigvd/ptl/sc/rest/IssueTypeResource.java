f6b557032cdab6dd132132135e1eedc822ad2b24
umelec <info@fclerc.ch>
2015-02-18 18:26:29 +0100

    IssueType works

--- src/main/java/ch/heigvd/ptl/sc/rest/IssueTypeResource.java
+++ src/main/java/ch/heigvd/ptl/sc/rest/IssueTypeResource.java
@@ -1,9 +1,10 @@
 package ch.heigvd.ptl.sc.rest;
 
+
 import ch.heigvd.ptl.sc.CityEngagementException;
-import ch.heigvd.ptl.sc.persistence.IssueTypeRepository;
 import ch.heigvd.ptl.sc.converter.IssueTypeConverter;
 import ch.heigvd.ptl.sc.model.IssueType;
+import ch.heigvd.ptl.sc.persistence.IssueTypeRepository;
 import ch.heigvd.ptl.sc.to.IssueTypeTO;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -22,30 +23,62 @@ import org.springframework.stereotype.Component;
 @Path("/issuestypes")
 public class IssueTypeResource {
 	@Autowired
-	private IssueTypeRepository issueTypeRepository;
+	private IssueTypeRepository IssueTypeRepository;
 	
 	@Autowired
-	private IssueTypeConverter issueTypeConverter;
+	private IssueTypeConverter IssueTypeConverter;
 
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response findAll() {
-            
-		return Response.ok(issueTypeConverter.convertSourceToTarget(issueTypeRepository.findAll())).build();
+		return Response.ok(IssueTypeConverter.convertSourceToTarget(IssueTypeRepository.findAll())).build();
 	}
-        
-        @GET
+
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response create(IssueTypeTO IssueTypeTO) {
+		IssueType issueType = IssueTypeRepository.save(IssueTypeConverter.convertTargetToSource(IssueTypeTO));
+		
+		return Response.ok(IssueTypeConverter.convertSourceToTarget(issueType)).status(201).build();
+	}
+	
+	@GET
 	@Path("{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response read(@PathParam("id") String id) {
-		IssueType issueTpye = issueTypeRepository.findOne(id);
+		IssueType issueType = IssueTypeRepository.findOne(id);
 		
-		if (issueTpye == null) {
+		if (IssueType == null) {
 			throw new CityEngagementException(404, "Model not found.");
 		}
 		
-		return Response.ok(issueTypeConverter.convertSourceToTarget(issueTpye)).build();
+		return Response.ok(IssueTypeConverter.convertSourceToTarget(issueType)).build();
 	}
-        
+	
+	@PUT
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response update(@PathParam("id") String id, IssueTypeTO IssueTypeTO) {
+		IssueType issueType = IssueTypeRepository.findOne(id);
+
+		if (issueType == null) {
+			throw new CityEngagementException(404, "Model not found.");
+		}
+		
+		IssueTypeConverter.fillSourceFromTarget(issueType, IssueTypeTO);
+		
+		issueType = IssueTypeRepository.save(issueType);
+
+		return Response.ok(IssueTypeConverter.convertSourceToTarget(issueType)).build();
+	}
+
+	@DELETE
+	@Path("/{id}")
+	public Response delete(@PathParam("id") String id) {
+		IssueTypeRepository.delete(id);
+		return Response.ok().status(204).build();
+	}
 }
