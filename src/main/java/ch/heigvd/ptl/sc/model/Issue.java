00a24aaa184dabfaefc0ad32293ec4df801d5bea
umelec <info@fclerc.ch>
2015-02-19 21:39:11 +0100

    Issue v1

--- src/main/java/ch/heigvd/ptl/sc/model/Issue.java
+++ src/main/java/ch/heigvd/ptl/sc/model/Issue.java
@@ -13,20 +13,96 @@ public class Issue {
 	private String issueType;
 	private String description;
         private String geoCoordonnee;
-        private String statu;
+        private String status;
         
         
+        /**Objet autheur lié**/
         public String getAuthor() {
 		return author; 
 	}
         
+        public void setAuthor(String author) {
+		this.author = author;
+	}
+        
+        /** Un ou plusieurs issueType **/
         public String getIssueType(){
             return issueType;
         }
+        
+        public void setIssueType(String issueType){
+             this.issueType = issueType;
+        }
 	
         
+        public String getDescription(){
+            return description;
+        }
+        
+        public void setDescription(String description){
+             this.description  = description;
+        }
+        
+        
+        public String getGeoCoordonnee(){
+            return geoCoordonnee;
+        }
+        
+        public void setGeoCoordonnee(String geoCoordonnee){
+             this.geoCoordonnee = geoCoordonnee;
+        }
+        
+         public String getStatus(){
+            return geoCoordonnee;
+        }
+        
+        public void setStatus(String status){
+             this.status = status;
+        }
+	
+	        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        public String getId() {
+		return id;
+	}
 
 	
 	
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 	
 }
--- src/main/java/ch/heigvd/ptl/sc/to/IssueTO.java
+++ src/main/java/ch/heigvd/ptl/sc/to/IssueTO.java
@@ -5,5 +5,61 @@ import java.util.List;
 public class IssueTO {
 	private String id;
 	
+	private String author;
+	private String issueType;
+	private String description;
+        private String geoCoordonnee;
+        private String status;
 
+	private List<String> roles;
+	
+	public String getId() {
+		return id;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public String getIssueType() {
+		return issueType;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+        
+        public String getGeoCoordonnee() {
+		return geoCoordonnee;
+	}
+
+        public String getStatus() {
+		return status;
+	}
+        
+        /**Sera un object -> user **/
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public void setIssueType(String issueType) {
+		this.issueType = issueType;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+        
+        public void SetSeoCoordonnee(String geoCoordonnee) {
+		this.geoCoordonnee = geoCoordonnee;
+	}
+        
+        public void SetStatus(String status) {
+		this.status = status;
+	}
+        
+
+	public void setId(String id) {
+		this.id = id;
+	}
 }
